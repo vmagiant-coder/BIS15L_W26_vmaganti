3a7c4a0b9cedbadbd34ce84fb2d26855fa35ff0f
vmagiant-coder <vmaganti@ucdavis.edu>
2026-02-26 23:13:53 -0800

    hw 15

---
+++ lab15/elephants.R
@@ -0,0 +1,49 @@
+## Load the libraries
+
+library(tidyverse)
+library(janitor)
+library(shiny)
+library(shinydashboard)
+
+library(shiny)
+
+ui <- dashboardPage(
+  
+  dashboardHeader(title = "Elephant Age and Height by Sex"),
+  
+  dashboardSidebar(
+    selectInput("x",
+                "Select Variable",
+                choices = c("age",
+                            "height"),
+                selected =  "age")
+    
+    
+  ),
+  
+  dashboardBody(
+    
+    plotOutput("plot", width = "600px", height = "500px")
+    
+  )
+  
+)
+
+server <- function(input, output, session) {
+  
+  output$plot <- renderPlot({
+    
+    elephants %>%
+      filter(sex != "NA") %>%
+      ggplot(aes(x=sex,
+                 y=.data[[input$x]],
+                 fill = sex)) +
+      geom_boxplot(alpha = 0.75) +
+      labs(title = "Range by Sex",
+           x = "Sex",
+           y = input$x) +
+      theme_minimal()
+  })
+}
+
+shinyApp(ui, server)
